b29be296d96a13d68a81af1771797a95ec992c85
lndwaru <lndwaru@LNDWARU-RO.ke.oracle.com>
2019-06-24 23:53:50 +0300

    add files

--- index.js
+++ index.js
@@ -7,6 +7,15 @@ app.get('/',function(req,res){
     res.send('hello world!');
     })
 
+app.get('/list',function(req,res){
+res.send("Name Listing");
+})
+
+
+
+
+
+    ///serve 
     app.listen(app.get('port'), function(){
 
         console.log("app listening at port " + app.get('port') );
